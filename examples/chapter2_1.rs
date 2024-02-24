896b6a7d22166542fd7d6be8c8d9845b69c53700
BillYao9 <billyao9@gmail.com>
2024-02-24 15:49:00 +0800

    0224-2

--- examples/chapter2_1.rs
+++ examples/chapter2_1.rs
@@ -6,12 +6,20 @@ fn main() {
     for _ in 1..100 {
         n *= 2;
         let mut a1 = Vec::with_capacity(n);
-        let mut a2 = Vec::with_capacity(n);
         for _ in 0..n {
             let v = rand::thread_rng().gen_range(1..=n);
             a1.push(v);
-            a2.push(v);
         }
+        // for j in 1..n {
+        //     let temp = a1[j];
+        //     let mut i: i32 = (j - 1) as i32;
+        //     while i >= 0 && a1[i as usize] > temp {
+        //         a1[(i + 1) as usize] = a1[i as usize];
+        //         i -= 1;
+        //     }
+        //     a1[(i + 1) as usize] = temp;
+        // }
+        // a1.reverse();
         let start = Instant::now();
         for j in 1..n {
             let temp = a1[j];
--- examples/chapter2_3.rs
+++ examples/chapter2_3.rs
@@ -10,8 +10,12 @@ fn main() {
             let v: usize = rand::thread_rng().gen_range(1..=n);
             a1.push(v);
         }
+        // merge_sort(&mut a1, 0, n);
+        // a1.reverse();
         let start = Instant::now();
+
         merge_sort(&mut a1, 0, n);
+
         let end = Instant::now();
         let duration = end.duration_since(start);
         println!("长度为 {n} 的排序运行了 {} 毫秒", duration.as_millis());
@@ -24,7 +28,6 @@ fn merge_sort(a1: &mut Vec<usize>, p: usize, r: usize) {
     // println!("a1 : {:?}", a1);
     // println!("p : {}", p);
     // println!("r : {}", r);
-
     if p < r - 1 {
         let q = (p + r) / 2;
         //println!("q : {}", q);
@@ -33,19 +36,17 @@ fn merge_sort(a1: &mut Vec<usize>, p: usize, r: usize) {
         merge(a1, p, q, r);
     }
 }
-fn merge(a1: &mut Vec<usize>, p: usize, q: usize, r: usize) {
+fn merge(a1: &mut [usize], p: usize, q: usize, r: usize) {
     let len1 = q - p;
     let len2 = r - q;
     let mut la = Vec::with_capacity(len1 + 1);
     let mut ra = Vec::with_capacity(len2 + 1);
     //println!("{:?}", a1);
-
     for i in 0..len1 {
         la.push(a1[p + i]);
     }
     la.push(usize::MAX);
     //println!("{:?}", la);
-
     for i in 0..len2 {
         ra.push(a1[q + i]);
     }
